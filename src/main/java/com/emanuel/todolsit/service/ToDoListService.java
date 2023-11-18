fc3fc839086a1fa5d0903368a05cb85ba64314fe
Emanuel <emanuelolvr@outlook.com>
2023-11-18 16:52:00 -0300

    feat: create method save and create

--- src/main/java/com/emanuel/todolsit/service/ToDoListService.java
+++ src/main/java/com/emanuel/todolsit/service/ToDoListService.java
@@ -1,11 +1,11 @@
 package com.emanuel.todolsit.service;
 
 import com.emanuel.todolsit.domains.ToDoList;
+import com.emanuel.todolsit.dto.ToDoListDTO;
 import com.emanuel.todolsit.repository.ToDoListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.config.Task;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,8 +14,14 @@ public class ToDoListService {
     @Autowired
     private ToDoListRepository toDoListRepository;
 
-    public ResponseEntity<ToDoList> saveTaks(ToDoList toDoList){
+    public ResponseEntity<ToDoList> saveTask(ToDoList toDoList){
         this.toDoListRepository.save(toDoList);
         return new ResponseEntity<>(toDoList, HttpStatus.OK);
+    }
+
+    public ToDoList createTask(ToDoListDTO data){
+        ToDoList newTask = new ToDoList();
+        this.saveTask(newTask);
+        return newTask;
     }
 }
