77dd4ecd92c3938dcf44ff3686f3a9e4f09b1d81
Emanuel <emanuelolvr@outlook.com>
2023-11-18 17:12:11 -0300

    feat: create post mapping

---
+++ src/main/java/com/emanuel/todolsit/controller/ToDoListController.java
@@ -0,0 +1,26 @@
+package com.emanuel.todolsit.controller;
+
+import com.emanuel.todolsit.domains.ToDoList;
+import com.emanuel.todolsit.dto.ToDoListDTO;
+import com.emanuel.todolsit.service.ToDoListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/todolist")
+public class ToDoListController {
+
+    @Autowired
+    private ToDoListService toDoListService;
+
+    @PostMapping
+    public ResponseEntity<ToDoList> createTask(@RequestBody ToDoListDTO data){
+        ToDoList newTask = this.toDoListService.createTask(data);
+        return new ResponseEntity<>(newTask, HttpStatus.OK);
+    }
+}
